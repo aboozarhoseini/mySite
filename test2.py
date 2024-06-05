375960835baaa3133a784aa68cdb178852857070
aboozar <aboozarh4@gmail.com>
2024-06-05 17:05:52 +0330

    create fiel

---
+++ test2.py
@@ -0,0 +1,38 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * self.length + 2 * self.width
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
+
+    def area(self):
+        return super().area() * 10
+
+
+class Cube(Square):
+
+    def area(self):
+        return super(Square, self).area()
+
+
+sq = Square(10)
+print(sq.area())
+print(sq.perimeter())
+
+print('-----------------')
+
+cu = Cube(10)
+
+print(cu.area())
+print(cu.perimeter())
+
+print(isinstance(sq, Square))
